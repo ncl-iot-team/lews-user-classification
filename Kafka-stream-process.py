f765b351bcc1f09848495aa5998eecfc066cf2ec
Nipun Balan Thekkummal <nipunbalan@gmail.com>
2020-03-19 00:31:00 +0000

    Removed hardcoded classes

--- Kafka-stream-process.py
+++ Kafka-stream-process.py
@@ -8,19 +8,37 @@ import csv
 import re
 
 
+filename=os.environ.get("CLASS_FILE_NAME","User_classification_data.csv")
+kafka_src_server = os.environ.get("KAFKA_SOURCE_BOOTSTRAP_SERVERS","localhost:9092").split(",")
+kafka_src_topic = os.environ.get('KAFKA_SOURCE_TOPIC', 't_topic1')
+kafka_tgt_server = os.environ.get("KAFKA_TARGET_BOOTSTRAP_SERVERS","localhost:9092").split(",")
+kafka_tgt_topic = os.environ.get('KAFKA_TARGET_TOPIC','t_topic2')
+proc_name = os.environ.get('MODULE_NAME','USER_CLASSIFICATION')
+
+print("Environment variables:")
+print(f"CLASS_FILE_NAME = {filename}")
+print(f"KAFKA_SOURCE_BOOTSTRAP_SERVERS = {kafka_src_server}")
+print(f"KAFKA_SOURCE_TOPIC = {kafka_src_topic}")
+print(f"KAFKA_TARGET_BOOTSTRAP_SERVERS = {kafka_tgt_server}")
+print(f"KAFKA_TARGET_TOPIC = {kafka_tgt_topic}")
+print(f"MODULE_NAME = {proc_name}")
+
+
+
 #--------------- Template Code, Avoid changing anything in this section --------------------------# 
 class AbstractKafkaInStreamProcessor(ABC):
         
     def produce_data_kafka(self,record) -> None:
-      
-      self.producer.send(topic=self.target_topic,value=record)
-      #print("Processed Record Sent")
+
+      self.producer.send(topic=kafka_tgt_topic,value=record)
+
+      print("Processed Record Sent")
 
 
 
     @abstractmethod
-    def process_data(self,record) -> None:
-        
+    def process_data(self,record) -> None:        
+
         return record
 
 
@@ -29,11 +47,12 @@ class AbstractKafkaInStreamProcessor(ABC):
 
         for message in self.consumer:
             
-            try:
+          #  try:
                 self.processed_record = self.process_data(message)
+
                 self.produce_data_kafka(self.processed_record)
-            except:
-                print("Skipping Record..")
+         #   except:
+         #       print("Skipping Record..")
 
         
             
@@ -41,24 +60,21 @@ class AbstractKafkaInStreamProcessor(ABC):
 
 
 
-    def __init__(self,processor_name,source_topic,target_topic):
+    def __init__(self):
+        
+        print("Initializing Kafka Consumer")
+        
+        self.consumer = KafkaConsumer(kafka_src_topic, group_id = proc_name, bootstrap_servers = kafka_src_server,value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 
-        self.processor_name = processor_name
-        
-        self.source_topic = source_topic
-        
-        self.target_topic = target_topic
-        
-        self.bootstrap_servers = os.getenv('KAFKA_BROKER','host.docker.internal:9092')
-        #self.bootstrap_servers = 'localhost:9092'
-        
-        print("Initializing Kafka In-Stream Processor Module")
-        
-        self.consumer = KafkaConsumer(source_topic,group_id = self.processor_name, bootstrap_servers = self.bootstrap_servers,value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+        print("Initializing Kafka Producer") 
+        self.producer = KafkaProducer(bootstrap_servers = kafka_tgt_server, value_serializer = lambda v: json.dumps(v).encode('utf-8'))
 
-       # self.producer = KafkaProducer(bootstrap_servers=self.bootstrap_servers)
+        with open(filename,'r')as f:
+                reader = csv.reader(f, delimiter=",")
+                self.class_matrix = []
+                for i, class_list in enumerate(reader):
+                    self.class_matrix.append(class_list)
 
-        self.producer = KafkaProducer(bootstrap_servers=self.bootstrap_servers, value_serializer = lambda v: json.dumps(v).encode('utf-8'))
 
 
 
@@ -83,51 +99,38 @@ class ConKafkaInStreamProcessor(AbstractKafkaInStreamProcessor):
      def process_data(self,message) -> None:
 #------------------- Add module Logic in this section ---------------------#
         try:
-            list1 = []
-            list2 = []
-            list3 = []
-            list4 = []
-            with open('User_classification_data.csv','r')as f:
-                reader = csv.reader(f, delimiter=",")
-                for i, line in enumerate(reader):
-                    if(i==1):
-                        list1 = line
-                    elif(i==2):
-                        list2 = line
-                    elif(i==3):
-                        list3 = line
-                    else:
-                        list4 = line
-
-
             # To get value from a field (Example)
-            util.json_util = util.JsonDataUtil(message.value)
+            json_util = util.JsonDataUtil(message.value)
             
+            get_user = json_util.get_value("user")
 
-            tweet_text = util.json_util.get_value("text")
-            get_user = util.json_util.get_value("user")
             data = get_user["screen_name"]
-            #Do Processing
-            if(data in list1):
-                util.json_util.add_metadata("User classification","news_agencies")
-            elif(data in list2):
-                util.json_util.add_metadata("User classification","govt_agencies")
-            elif(data in list3):
-                util.json_util.add_metadata("User classification","ngo")
-            else:
-                util.json_util.add_metadata("User classification","other")
+
+            classes = []
+
+            for class_list in self.class_matrix:
+
+                if(data in class_list[1:]):
+
+                    classes.append(class_list[0])
+
+                    print(f"User:{data} | Class identified: {class_list[0]}")
+
+
+
+            if len(classes)>0:
+
+                json_util.add_metadata("lews_meta_user_class",json.dumps(classes))
+
 
         except:
             print("Invalid Tweet Record.. Skipping")
             raise
 
         #Get the processed record with metadata added
-        processes_message = util.json_util.get_json() 
+        processes_message = json_util.get_json() 
 #---------------------- Add module logic in this section (End) ----------------------#
         return processes_message
-
-
-
 
 
 if __name__ == "__main__":
@@ -135,8 +138,4 @@ if __name__ == "__main__":
     #processor_name: Unique processor name for the module, 
     #source_topic: Topic from which the module should accept the record to be processed, 
     # target_topic: Topic to which the module publishes the processed record
-   s_topic = os.getenv('MODULE_SRC_TOPIC','lews-twitter')
-   t_topic = os.getenv('MODULE_TGT_TOPIC','t_topic')
-   proc_name = os.getenv('MODULE_NAME','Module01')  
-   
-   run(ConKafkaInStreamProcessor(processor_name=proc_name, source_topic=s_topic, target_topic=t_topic))
+   run(ConKafkaInStreamProcessor())
--- LEWSJsonUtil.py
+++ LEWSJsonUtil.py
@@ -2,45 +2,32 @@ import json
 
 class JsonDataUtil():
 
-    def __init__(self,json_string):
+    def __init__(self,value_string):
 
-        self.json_data = json_string
+        self.json_data = json.loads(value_string)
 
+      #  self.new_json_data = {}
+
+#        if 'lews_metadata' not in self.json_data:
+
+#            self.new_json_data['raw_data'] = self.json_data
+ #           self.new_json_data['lews_metadata'] = {}
+
+    def retain_fields(self,fields):
         self.new_json_data = {}
-
-        if 'lews_metadata' not in self.json_data:
-
-            self.new_json_data['raw_data'] = self.json_data
-            self.new_json_data['lews_metadata'] = {}
+        for field in fields:
+            self.new_json_data[field]=self.json_data[field]
+        self.json_data = self.new_json_data
 
     def get_value(self,field):
-        return self.new_json_data['raw_data'][field]
-      #  print(self.json_data)
+#        return self.new_json_data['raw_data'][field]
+        return self.json_data[field]
 
     def add_metadata(self,key,value):
-        self.new_json_data['lews_metadata'][key]=value
+#        self.new_json_data['lews_metadata'][key]=json.loads(value)
+        self.json_data[key]=json.loads(value)
 
-        #print(self.json_data)
 
     def get_json(self):
-        return self.new_json_data
-
-
-    
-# json_util = JsonDataUtil('{"raw_data": { "text": "Success", "text2": "New Success"}, "lews_metadata": { "new":"1" } }')
-
-# print(json_util.get_value("text"))
-# print(json_util.get_value("text2"))
-
-# json_util = JsonDataUtil('{ "text": "Success2", "text2": "New Success2"}')
-
-# print(json_util.get_value("text"))
-# print(json_util.get_value("text2"))
-
-
-# json_util.add_metadata('Newkey','Newvalue')
-
-# json_util.add_metadata('Lat','1.0324')
-
-# json_util.add_metadata('Long','-2423')
-
+#        return json.dumps(self.new_json_data)
+        return json.dumps(self.json_data)
